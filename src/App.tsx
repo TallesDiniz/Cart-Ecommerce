2e6ed25978fea2cde603dbc7619117e9a22064c9
Talles Diniz <tallessdiniz@gmail.com>
2026-03-30 00:03:59 -0300

    fix: correções pequenas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Home } from './pages/Home';
 import { Cart } from './pages/Cart';
 import { createBrowserRouter } from 'react-router-dom';
 import { Layout } from './components/Layout';
+import { Product } from './pages/Product';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: '/cart',
         element: <Cart/>
+      },
+      {
+        path: '/product/:id',
+        element: <Product/>
       }
     ]
   }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './App'
 import { RouterProvider } from 'react-router-dom';
-
+import { Toaster } from 'react-hot-toast';
 import CartProvider from './context/CartContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CartProvider>
+      <Toaster
+        position='top-center'
+        reverseOrder={false}
+      />
       <RouterProvider router={router}/>
     </CartProvider>
   </StrictMode>,
