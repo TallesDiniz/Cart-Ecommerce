7c9b6500f61e2e1ebadc45261b9781e361c2d2fc
Talles Diniz <tallessdiniz@gmail.com>
2026-03-30 00:20:26 -0300

    fix: troca da URL para produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 //npx json-server db.json
 export const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://my-json-server.typicode.com/TallesDiniz/Cart-Ecommerce'
 }) 
