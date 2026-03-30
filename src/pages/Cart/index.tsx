1bd5f480a8941857f0109c9a0fc16854846ab7ea
Talles Diniz <tallessdiniz@gmail.com>
2026-03-29 23:55:16 -0300

    feature: adição das funcionalidades carrinho e estilização

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,32 +1,50 @@
+import { useContext } from 'react'
+import { CartContext } from '../../context/CartContext'
+import { Link } from 'react-router-dom' 
+
 export function Cart() {
+    const { cart, total, addItemCart, removeItemCart } = useContext(CartContext)
+
     return (
         <div className='w-full max-w-7xl mx-auto '>
             <h1 className='font-medium text-2xl text-center my-4'>Carrinho de compras</h1>
 
-            <section className='flex items-center justify-between border-b-2 border-gray-300'>
-                <img
-                    className='w-28'
-                    src="https://images.unsplash.com/photo-1503602642458-232111445657?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-                    alt="Logo produto"
-                />
-
-                <strong>Preço: R$500</strong>
-
-                <div className='flex items-center justify-center gap-3'>
-                    <button className='bg-slate-600 rounded px-2 text-white font-medium flex items-center justify-center'>
-                        -
-                    </button>
-                    <span>2</span>
-                    <button className='bg-slate-600 rounded px-2 text-white font-medium flex items-center justify-center'>
-                        +
-                    </button>
-                    <strong className='float-right'>
-                        subtotal: R$500
-                    </strong>
+            {cart.length === 0 && (
+                <div className='flex flex-col items-center justify-center'>
+                    <p className='font-medium'>Ops seu carrinho está vazio...</p>
+                    <Link to='/' className='bg-slate-600 my-3 p-1 px-3 text-white font-medium rounded'>
+                        Acessar produtos
+                    </Link>
                 </div>
-            </section>
+            )}
 
-            <p className='font-bold mt-4'>Total: R$500</p>
+            {cart.map((item) => (
+                <section key={item.id} className='flex items-center justify-between border-b-2 border-gray-300'>
+                    <img
+                        className='w-28'
+                        src={item.cover}
+                        alt={item.title}
+                    />
+
+                    <strong>Preço: {item.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</strong>
+
+                    <div className='flex items-center justify-center gap-3'>
+                        <button onClick={() => removeItemCart(item)} className='bg-slate-600 cursor-pointer rounded px-2 text-white font-medium flex items-center justify-center'>
+                            -
+                        </button>
+                        {item.amount}
+                        <button onClick={() => addItemCart(item)} className='bg-slate-600 cursor-pointer rounded px-2 text-white font-medium flex items-center justify-center'>
+                            +
+                        </button>
+                        <strong className='float-right'>
+                            subtotal: {item.total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
+                        </strong>
+                    </div>
+                </section>
+            ))}
+
+            {cart.length !== 0 && 
+            <p className='font-bold mt-4'>Total: {total}</p>}
         </div>
     )
 }
